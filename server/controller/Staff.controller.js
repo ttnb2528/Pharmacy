59eef529268329b09fb6a818d73f73f72a4d9a72
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-12-30 16:15:28 +0700

    CRUD staff

--- server/controller/Staff.controller.js
+++ server/controller/Staff.controller.js
@@ -1,3 +1,4 @@
+import Joi from "joi";
 import asyncHandler from "../middleware/asyncHandler.js";
 import Staff from "../model/Staff.model.js";
 import { StatusCode } from "../utils/constants.js";
@@ -63,3 +64,103 @@ export const createStaff = asyncHandler(async (req, res) => {
     res.json(jsonGenerate(StatusCode.SERVER_ERROR, error.message));
   }
 });
+
+export const getStaffs = asyncHandler(async (req, res) => {
+  try {
+    const staffs = await Staff.find();
+
+    res.json(
+      jsonGenerate(StatusCode.OK, "Lấy danh sách nhân viên thành công", staffs)
+    );
+  } catch (error) {
+    res.json(jsonGenerate(StatusCode.SERVER_ERROR, error.message));
+  }
+});
+
+export const getStaff = asyncHandler(async (req, res) => {
+  try {
+    const staff = await Staff.findById(req.params.id);
+
+    if (!staff) {
+      return res.json(
+        jsonGenerate(StatusCode.NOT_FOUND, "Không tìm thấy nhân viên")
+      );
+    }
+
+    res.json(jsonGenerate(StatusCode.OK, "Lấy nhân viên thành công", staff));
+  } catch (error) {
+    res.json(jsonGenerate(StatusCode.SERVER_ERROR, error.message));
+  }
+});
+
+export const updateStaff = asyncHandler(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const staff = await Staff.findById(id);
+
+    if (!staff) {
+      return res.json(
+        jsonGenerate(StatusCode.NOT_FOUND, "Không tìm thấy nhân viên")
+      );
+    }
+
+    const { error } = validate(req.body);
+
+    if (error) {
+      return res.json(
+        jsonGenerate(StatusCode.BAD_REQUEST, error.details[0].message)
+      );
+    }
+
+    const staffExits = await Staff.findOne({ username: req.body.username });
+
+    if (staffExits) {
+      return res.json(
+        jsonGenerate(StatusCode.BAD_REQUEST, "Username đã tồn tại")
+      );
+    }
+
+    await Staff.findByIdAndUpdate(id, req.body);
+
+    res.json(jsonGenerate(StatusCode.OK, "Cập nhật nhân viên thành công"));
+  } catch (error) {
+    res.json(jsonGenerate(StatusCode.SERVER_ERROR, error.message));
+  }
+});
+
+export const deleteStaff = asyncHandler(async (req, res) => {
+  try {
+    const staff = await Staff.findById(req.params.id);
+
+    if (!staff) {
+      return res.json(
+        jsonGenerate(StatusCode.NOT_FOUND, "Không tìm thấy nhân viên")
+      );
+    }
+
+    await Staff.findByIdAndDelete(req.params.id);
+
+    res.json(jsonGenerate(StatusCode.OK, "Xóa nhân viên thành công"));
+  } catch (error) {
+    res.json(jsonGenerate(StatusCode.SERVER_ERROR, error.message));
+  }
+});
+
+const validate = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().required().label("Tên nhân viên"),
+    gender: Joi.string().required().label("Giới tính"),
+    date: Joi.date().required().label("Ngày sinh"),
+    phone: Joi.string().required().label("Số điện thoại"),
+    address: Joi.string().required().label("Địa chỉ"),
+    workDate: Joi.string().required().label("Ngày vào làm"),
+  })
+    .messages({
+      "string.empty": "{#label} không được để trống",
+      "any.required": "{#label} là bắt buộc",
+      "string.base": "{#label} phải là chuỗi ký tự",
+    })
+    .unknown(true);
+
+  return schema.validate(data);
+};
--- server/routes/Staff.route.js
+++ server/routes/Staff.route.js
@@ -1,8 +1,18 @@
-import express from 'express';
-import { createStaff } from '../controller/Staff.controller.js';
+import express from "express";
+import {
+  createStaff,
+  deleteStaff,
+  getStaff,
+  getStaffs,
+  updateStaff,
+} from "../controller/Staff.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createStaff);
+router.get("/getStaffs", getStaffs);
+router.get("/getStaff/:id", getStaff);
+router.put("/updateStaff/:id", updateStaff);
+router.delete("/deleteStaff/:id", deleteStaff);
 
 export default router;
