8fe1e419a43090538182b2dba6fcc5c492bd655a
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-12-31 23:03:28 +0700

    enviroment setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
