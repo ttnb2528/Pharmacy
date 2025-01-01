b5184e78d2ea9add2b26aa12e5650bee006f7008
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2025-01-01 21:11:56 +0700

    change port

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
 const databaseURL = process.env.DATABASE_URL;
 
 app.use(
