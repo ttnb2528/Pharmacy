b783f72ec0a63152faadb887678296e1224113a9
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2025-01-02 18:17:49 +0700

    Home Suggest

--- client/src/pages/Client/Home/Home.jsx
+++ client/src/pages/Client/Home/Home.jsx
@@ -2,6 +2,7 @@ import HomeBanner from "./components/HomeBanner.jsx";
 import HomeBestSelling from "./components/HomeBestSelling.jsx";
 import HomeDeal from "./components/HomeDeal.jsx";
 import HomeProductForGroup from "./components/HomeProductForGroup.jsx";
+import HomeSuggest from "./components/HomeSuggest.jsx";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <HomeDeal />
       <HomeBestSelling />
       <HomeProductForGroup />
+      <HomeSuggest />
     </div>
   );
 };
---
+++ client/src/pages/Client/Home/components/HomeSuggest.jsx
@@ -0,0 +1,63 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Item from "@/pages/component/ProductItem/Item.jsx";
+const HomeSuggest = () => {
+  const suggestProducts = [1, 2, 3, 4, 5];
+
+  var settings = {
+    dots: false,
+    lazyLoad: true,
+    slidesToShow: 5,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: false,
+          lazyLoad: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          dots: false,
+          lazyLoad: true,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          lazyLoad: true,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div>
+      <div className="product_carousel bg-[#dcefe3] rounded-lg px-3 mb-10">
+        <div className="px-6 pt-6 pb-9 ">
+          <div className="product_carousel-header mb-5">
+            <span className="product_carousel-title text-3xl font-bold">
+              Gợi ý hôm nay
+            </span>
+          </div>
+
+          <div className="slider-container">
+            <Slider {...settings}>
+              {suggestProducts.map((product, i) => {
+                return <Item key={i} />;
+              })}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSuggest;
