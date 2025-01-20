6eb7de1b75ab3e24f80a6ef0824549bb2e569caa
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2025-01-20 15:43:35 +0700

    add logic cart context

--- client/src/context/Pharmacy.context.jsx
+++ client/src/context/Pharmacy.context.jsx
@@ -1,21 +1,29 @@
-import { GET_ALL_ADDRESSES_ROUTE, GET_USER_INFO } from "@/API/index.api.js";
+import {
+  GET_ALL_ADDRESSES_ROUTE,
+  GET_ALL_PRODUCTS_ROUTE,
+  GET_COUPONS_ROUTE,
+  GET_USER_INFO,
+} from "@/API/index.api.js";
 import { apiClient } from "@/lib/api-client.js";
 import { createContext, useEffect, useState } from "react";
 
 export const PharmacyContext = createContext(null);
 
-// const getDefaultCart = () => {
-//   let cart = {};
-//   for (let index = 0; index < 300 + 1; index++) {
-//     cart[index] = 0;
-//   }
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < 300 + 1; index++) {
+    cart[index] = 0;
+  }
 
-//   return cart;
-// };
+  return cart;
+};
 
 const PharmacyContextProvider = (props) => {
   const [userData, setUserData] = useState(null);
   const [addressData, setAddressData] = useState([]);
+  const [couponData, setCouponData] = useState([]);
+  const [cart, setCart] = useState(getDefaultCart());
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -23,6 +31,7 @@ const PharmacyContextProvider = (props) => {
         const resUser = await apiClient.get(GET_USER_INFO);
         if (resUser.status === 200) {
           setUserData(resUser.data.data);
+          setCart(resUser.data.data.accountId.cartData);
         } else if (resUser.status === 500) {
           setUserData(null);
         }
@@ -44,19 +53,64 @@ const PharmacyContextProvider = (props) => {
       }
     };
 
+    const fetchCouponData = async () => {
+      try {
+        const resCoupon = await apiClient.get(GET_COUPONS_ROUTE);
+        if (resCoupon.status === 200) {
+          setCouponData(resCoupon.data.data);
+        } else if (resCoupon.status === 500) {
+          setCouponData(null);
+        }
+      } catch (error) {
+        console.error("Lỗi khi lấy thông tin mã giảm giá:", error);
+      }
+    };
+
+    const fetchAllProducts = async () => {
+      try {
+        const resProducts = await apiClient.get(GET_ALL_PRODUCTS_ROUTE);
+        if (resProducts.status === 200) {
+          setAllProducts(resProducts.data.data);
+        } else if (resProducts.status === 500) {
+          setAllProducts(null);
+        }
+      } catch (error) {
+        console.error("Lỗi khi lấy thông tin sản phẩm:", error);
+      }
+    };
+
     fetchUserData();
     fetchAddressData();
+    fetchCouponData();
+    fetchAllProducts();
   }, []);
 
   const updateUserData = (newUserData) => {
     setUserData(newUserData);
   };
 
+  const CalculateTotalItems = (carts) => {
+    let totalItems = 0;
+
+    for (const item in carts) {
+      if (carts[item] > 0) {
+        totalItems += carts[item];
+      }
+    }
+
+    return totalItems;
+  };
+
   const contextValue = {
     userData,
     updateUserData,
     addressData,
-    setAddressData 
+    setAddressData,
+    couponData,
+    allProducts,
+    cart,
+    setCart,
+    CalculateTotalItems,
   };
 
   return (
