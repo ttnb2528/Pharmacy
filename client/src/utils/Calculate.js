085b3ca5f2fb52a8262d1a13a466ee11e89481bf
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2025-01-30 14:13:53 +0700

    add more func calculate

--- client/src/utils/Calculate.js
+++ client/src/utils/Calculate.js
@@ -17,3 +17,49 @@ export const handleRenderPriceWithCoupon = (coupon) => {
 export const CalculatePointEarned = (price, sale) => {
   return CalculateProductWithSale(price, sale) / 100;
 };
+
+export const CalculateRemainingAccumulated = (rank, totalSpending) => {
+  switch (rank) {
+    case "Bạc":
+      if (totalSpending > 4000000) return 0;
+      return 4000000 - totalSpending;
+    case "Vàng":
+      if (totalSpending > 8000000) return 0;
+      return 8000000 - totalSpending;
+    case "Kim cương":
+      return 0;
+    default:
+      return 0;
+  }
+};
+
+export const CalculatePercentProgress = (rank, totalSpending) => {
+  switch (rank) {
+    case "Bạc":
+      if (totalSpending > 4000000) return 100;
+      return (totalSpending / 4000000) * 100;
+    case "Vàng":
+      if (totalSpending > 8000000) return 100;
+      return (totalSpending / 8000000) * 100;
+    case "Kim cương":
+      return 100;
+    default:
+      return 0;
+  }
+};
+
+export const CalculatePercentProgressSilver = (rank, totalSpending) => {
+  if (rank === "Bạc") {
+    if (totalSpending > 4000000) return 100;
+    return (totalSpending / 4000000) * 100;
+  }
+  return;
+};
+
+export const CalculatePercentProgressGold = (rank, totalSpending) => {
+  if (rank === "Vàng") {
+    if (totalSpending > 8000000) return 100;
+    return (totalSpending / 8000000) * 100;
+  }
+  return;
+};
