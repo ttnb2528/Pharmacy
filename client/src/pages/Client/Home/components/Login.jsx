760500696d30f3aaa92d18086bebfe8fa1b69b14
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2025-03-29 16:45:09 +0700

    fix UI

--- client/src/pages/Client/Home/components/Login.jsx
+++ client/src/pages/Client/Home/components/Login.jsx
@@ -248,7 +248,7 @@ const Login = ({ close }) => {
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 bg-black/30 flex justify-center items-center z-[9999]">
       {isLoading && <Loading />}
-      <div className="grid focus-visible:outline-0 md:block px-4 md:px-6 pt-28 pb-6 left-0 fixed bottom-0 right-0 top-0 z-50 w-full gap-4 border bg-background shadow-lg duration-200 md:left-[50%] md:h-fit md:max-h-[95%] md:w-full md:translate-x-[-50%] md:rounded-lg overflow-x-hidden border-white md:top-[5%] md:max-w-[413px] md:translate-y-0 md:border-neutral-200 md:pt-6">
+      <div className="grid focus-visible:outline-0 md:block px-4 md:px-6 pt-28 pb-6 left-0 fixed bottom-0 right-0 top-0 z-50 w-full gap-4 border bg-background shadow-lg duration-200 md:left-[50%] md:h-fit md:max-h-[95%] md:w-full md:translate-x-[-50%] md:rounded-lg overflow-x-hidden border-white md:top-[15%] md:max-w-[413px] md:translate-y-0 md:border-neutral-200 md:pt-6">
         <IoIosClose
           className="absolute right-2 top-4 text-5xl"
           onClick={close}
